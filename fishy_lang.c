74ec64c6fbbe293f15134a5ca2c27cb98c57eeb6
Jared Taylor <jared.xyz@mailbox.org>
2020-02-16 01:58:04 -0500

    Linked list memory allocation print and deletion in fishy_lang

---
+++ fishy_lang.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define a int
+#define b main
+#define c void
+#define d {
+#define e }
+#define pr printf
+#define f ;
+#define r return
+#define zzz goto
+#define hook ->
+#define heck struct
+#define fish typedef
+#define time_to_die malloc
+#define fishychipz sizeof
+#define omae_wa_mou free
+#define sad *
+#define swimmy while
+#define is_confused_about =
+#define is_eating <
+#define dummy (
+#define thicc )
+#define hungers_and ++
+#define the_darkness 0
+
+fish heck n d
+    heck n* qtPI f
+    a v f
+e pool f
+
+a b dummy c thicc d
+    pool sad fishy is_confused_about time_to_die dummy fishychipz(pool) thicc f
+
+    pool sad fishgirly is_confused_about fishy f
+    a dsf is_confused_about the_darkness f
+    swimmy dummy dsf hungers_and is_eating 10 thicc d
+        fishy hook qtPI is_confused_about time_to_die dummy fishychipz(pool) thicc f
+        fishy is_confused_about fishy hook qtPI f
+    e
+
+    fishy is_confused_about fishgirly f
+    dsf is_confused_about the_darkness f
+    swimmy dummy fishy thicc d
+        fishy hook v is_confused_about dsf hungers_and f
+        fishy is_confused_about fishy hook qtPI f
+    e
+
+    fishy is_confused_about fishgirly f
+    swimmy dummy fishy thicc d
+        pr("Val: %d", fishy hook v) f
+        pool sad jealous_fishy is_confused_about fishy f
+        fishy is_confused_about fishy hook qtPI f
+        omae_wa_mou(jealous_fishy);
+    e
+    r -1 f
+e
